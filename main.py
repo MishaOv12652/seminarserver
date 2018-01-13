d0b918c3237181ad67ae174676519533afc22062
Misha <mishaovodenko@gmail.com>
2018-01-13 10:34:27 +0200

    main to run server

---
+++ main.py
@@ -0,0 +1,17 @@
+import ConnectionHandler
+
+
+def main():
+    while True:
+        port_num = input("Port? ")
+        try:
+            port_num = int(port_num)
+            break
+        except ValueError:
+            pass
+
+    ConnectionHandler.ThreadedServer('127.0.0.1', port_num).listen()
+
+
+if __name__ == '__main__':
+    main()
