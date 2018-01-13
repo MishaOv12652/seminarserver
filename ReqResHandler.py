8600b2497ee3c2497eff24ad768b9123304be4f8
Misha <mishaOvodenko@gmail.com>
2018-01-13 20:46:00 +0200

    added handle_num_args

--- ReqResHandler.py
+++ ReqResHandler.py
@@ -1,26 +1,38 @@
 import re
-import subprocess
 
 
 class ReqRes(object):
 
     def __init__(self, data):
         self.data = data
-    
+
     def handle_math_string_exp(self):
         result = eval(str(self.data))
+        print(result)
         return str(result)
 
     def handle_print(self):
         print_exp = re.search("print", str(self.data))
         return self.data[:print_exp.start()] + self.data[print_exp.end():]
 
+    def handle_num_args(self):
+        l_bracket = re.search('\(', str(self.data))
+        r_bracket = re.search('\)', str(self.data))
+        args_string = self.data[l_bracket.start() + 1:r_bracket.end() - 1]
+        args = []
+        for x in args_string.split(','):
+            args.append(x.strip)
+        return len(args)
+
     def handle_function(self):
         colons = re.search(":", str(self.data))
         def_exp = re.search("def", str(self.data))
         func_name = self.data[def_exp.end() + 1:colons.start()]
         exec self.data
-        exec func_name
+        if eval(func_name) is not None:
+            return eval(func_name)
+        else:
+            return self.handle_print()
 
     def handle_class(self):
         class_word_remove = re.search("class", str(self.data))
@@ -59,6 +71,7 @@ def main():
     print (ReqRes('def Hi(): print("Hi, I am Misha")').process_req())
     print (ReqRes('class Misha(object): fName = "Misha"').process_req())
     print (ReqRes('class Misha(): fName = "Misha"').process_req().f_name)
+    print (ReqRes('def add(x,y,a,p): return x+y').handle_args())
 
 
 if __name__ == '__main__':
