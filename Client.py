43a53b322f73b0695ab0ceb2afb0303c51626609
Misha <mishaovodenko@gmail.com>
2018-01-14 22:02:52 +0200

    some extra changes + client change to test encryption

--- Client.py
+++ Client.py
@@ -1,20 +1,28 @@
 import socket
-
+import cPickle
+from Crypto.PublicKey import RSA
 
 def main():
     host = '127.0.0.1'
     port = 5200
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mySocket.connect((host, port))
-    filepath = './cla.txt'  # './misha'
+    while True:
+        data = mySocket.recv(2048)
+        key = cPickle.loads(data)
+        pub_key = RSA.importKey(key)
+        break
+    filepath = './encDecTes.txt'#'./cla.txt'  # './misha'
     with open(filepath) as fp:
         line = fp.readline()
         cnt = 1
         while line:
             print("Line {}: {}".format(cnt, line.strip()))
+            line = cPickle.dumps(pub_key.encrypt(line, 32))
             mySocket.send(line)
             data = mySocket.recv(1024)
-            print ('Received from server: ' + data)
+            data = cPickle.loads(pub_key.decrypt(data))
+            print ('Received from server: ' + str(data))
             line = fp.readline()
             cnt += 1
 
