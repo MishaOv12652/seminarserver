623243f993718b18764914e08f86f2ba95b00530
Misha <mishaovodenko@gmail.com>
2018-01-14 13:50:49 +0200

    client read from file change + file.txt

--- Client.py
+++ Client.py
@@ -1,46 +1,23 @@
 import socket
 
-import socket
 
-
-def Main():
+def main():
     host = '127.0.0.1'
     port = 5200
-
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mySocket.connect((host, port))
+    filepath = './misha'
+    with open(filepath) as fp:
+        line = fp.readline()
+        cnt = 1
+        while line:
+            print("Line {}: {}".format(cnt, line.strip()))
+            mySocket.send(line)
+            data = mySocket.recv(1024)
+            print ('Received from server: ' + data)
+            line = fp.readline()
+            cnt += 1
 
-    message = input(" ? ")
-
-    while message != 'q':
-        mySocket.send(message)
-        data = mySocket.recv(1024)
-
-        print ('Received from server: ' + data)
-
-        message = input(" ? ")
-
-
-# mySocket.close()
 
 if __name__ == '__main__':
-    Main()
-# def main():
-#     host = '127.0.0.1'
-#     port = 5000
-#
-#     s = socket.socket()
-#     s.connect((host, port))
-#
-#     messege = input("-> ")
-#     while messege != 'q':
-#         s.send(messege)
-#         data = s.recv(1024)
-#         print("Recieved from server: " + data)
-#         messege = input("-> ")
-#
-#     s.close()
-#
-#
-# if __name__ == '__main__':
-#     main()
+    main()
