dafaa79dc23d05b4c7347b62a218535086e02315
Misha <mishaovodenko@gmail.com>
2018-01-14 11:01:32 +0200

    EncDecrypt fix

--- EncDycrpt.py
+++ EncDycrpt.py
@@ -1,10 +1,10 @@
 from Crypto.PublicKey import RSA
 import ast
 
+
 class EncDec(object):
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
         self.key = RSA.generate(2048)
         self.priv_pub_keys_dict = {}
 
@@ -14,24 +14,27 @@ class EncDec(object):
         self.priv_pub_keys_dict.update({"bin_priv_key": bin_priv_key})
         self.priv_pub_keys_dict.update({"bin_pub_key": bin_pub_key})
 
-    def encrypt_data(self):
+    def encrypt_data(self, data_to_encrypt):
         public_key = RSA.importKey(self.priv_pub_keys_dict["bin_pub_key"])
-        encrypted_msg = public_key.encrypt(str(self.data), 32)
+        encrypted_msg = public_key.encrypt(str(data_to_encrypt), 32)
         return encrypted_msg
 
-    def decrypt_data(self, encrypted_data):
+    def decrypt_data(self, data_to_decrypt):
         private_key = RSA.importKey(self.priv_pub_keys_dict["bin_priv_key"])
-        decrypted_msg = private_key.decrypt(ast.literal_eval(str(encrypted_data)))
+        decrypted_msg = private_key.decrypt(ast.literal_eval(str(data_to_decrypt)))
         return decrypted_msg
 
 
 def main():
-    data_to_encrypt = EncDec('def Misha(): print "misha"')
-    data_to_encrypt.create_private_public_key()
-    print ("data before encryption is Misha\n")
-    print ("data after encryption is :" + str(data_to_encrypt.encrypt_data()))
-    data_to_decrypt = data_to_encrypt.encrypt_data()
-    print ("data after decryption is :" + data_to_encrypt.decrypt_data(data_to_decrypt))
+    enc_dec_obj = EncDec()
+    enc_dec_obj.create_private_public_key()
+    data_to_encrypt = 'def Misha(): print "misha"'
+    encrypted_data = enc_dec_obj.encrypt_data(data_to_encrypt)
+
+    print ("data before encryption is :" + str(data_to_encrypt))
+    print ("data after encryption is: " + str(encrypted_data))
+    print ("data after decryption is: " + enc_dec_obj.decrypt_data(encrypted_data))
+
 
 if __name__ == '__main__':
     main()
